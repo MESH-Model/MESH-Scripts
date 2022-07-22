f8a0e5e16e4dcc28614ac0f269ebf09ae93ca4d6
Ala <66693414+AlaBahrami@users.noreply.github.com>
2022-07-22 13:31:52 -0600

    Update easymore_run.sh
    Editing the gdal version

--- Pre-Processing/vector_based_workflow/climate_forcing/sample_job/easymore_run.sh
+++ Pre-Processing/vector_based_workflow/climate_forcing/sample_job/easymore_run.sh
@@ -12,7 +12,7 @@
 
 # load modules for easymore dependencies
 module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3
-module load gdal/3.4.1 libspatialindex/1.8.5
+module load gdal/3.4.3 libspatialindex/1.8.5
 module load python/3.8.10 scipy-stack/2022a mpi4py/3.0.3
 
 # virtual environemnt 
