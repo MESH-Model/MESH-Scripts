52095f95f94d4650d21819be9a36fde471690094
cooperalbano <albano.caa@gmail.com>
2022-11-16 08:57:24 -0600

    Changed to relative paths in ddb file

--- Model_Workflow/vector_based_workflow/generate_drainage_database/create_drainage_database_exp.py
+++ Model_Workflow/vector_based_workflow/generate_drainage_database/create_drainage_database_exp.py
@@ -48,7 +48,7 @@ import time
 
 #Control file handling
 # Easy access to control file folder
-controlFolder = Path('/mnt/c/Users/5600x/Desktop/GWF/vector_based_workflow_CWARHM/0_control_files')
+controlFolder = Path('../0_control_files')
  
 # Store the name of the 'active' file in a variable
 controlFile = 'control_active.txt'
