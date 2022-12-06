e7f6ee1c07bcb316e25b2e22f642a8df99cd19d7
cooperalbano <albano.caa@gmail.com>
2022-12-06 13:13:22 -0600

    updated remapping to run as job array

--- Model_Workflow/vector_based_workflow/4_climate_forcing/easymore_remapping.py
+++ Model_Workflow/vector_based_workflow/4_climate_forcing/easymore_remapping.py
@@ -8,33 +8,35 @@ import os
 import numpy as np
 import xarray as xs
 import pandas as pd
+import sys
  
 # %% initializing EASYMORE object
 esmr = easymore()
  
 # specifying EASYMORE objects
 # name of the case; the temporary, remapping and remapped file names include case name            
-esmr.case_name                 = 'BowAtBanff'             
+esmr.case_name                 = 'PFAF78'             
 # temporary path that the EASYMORE generated GIS files and remapped file will be saved
-esmr.temp_dir                 = '../../forcing/TemporaryBowAtBanff/'
+esmr.temp_dir                 = 'temporary78_{}/'.format(sys.argv[1])
 # name of target shapefile that the source netcdf files should be remapped to
-esmr.target_shp               = '../../shape_file/catchment/BowAtBanff_cat.shp'
+esmr.target_shp               = '/project/6008034/Model_Output/MESH/NA_workflow/shape_file/catchment/cat_pfaf_78_MERIT_Hydro_v07_Basins_v01_bugfix1_WGS84.shp'
  
 # name of netCDF file(s); multiple files can be specified with *
-esmr.source_nc                 =  '../../forcing/Output78/PFAF78_remapped_1980-01-01-13-00-00.nc'
+#esmr.source_nc                 =  '/scratch/calbano/rdrs_output/1980/rdrsv2.1_1980_01.nc'
+esmr.source_nc                 =  '/scratch/calbano/rdrs_output/{}/rdrsv2.1_{}.nc'.format(sys.argv[1],sys.argv[1])
  
 # note : You can include forcing variables together if they are combined in one file or considere them separately
 # name of variables from source netCDF file(s) to be remapped
 esmr.var_names                 = ['RDRS_v2.1_A_PR0_SFC', 'RDRS_v2.1_P_FI_SFC',
-                                   'RDRS_v2.1_P_FB_SFC','RDRS_v2.1_P_TT_09944',
-                                   'RDRS_v2.1_P_UVC_09944','RDRS_v2.1_P_P0_SFC',
-                                   'RDRS_v2.1_P_HU_09944']
+                                  'RDRS_v2.1_P_FB_SFC','RDRS_v2.1_P_TT_09944',
+                                  'RDRS_v2.1_P_UVC_09944','RDRS_v2.1_P_P0_SFC',
+                                  'RDRS_v2.1_P_HU_09944']
  
-#esmr.var_names                 = ['RDRS_v2.1_P_HU_09944']
+#esmr.var_names                 = ['RDRS_v2_P_HU_09944']
 # rename the variables from source netCDF file(s) in the remapped files;
 # it will be the same as source if not provided
 # esmr.var_names_remapped      = ['PR','RDRS_v2_P_FI_SFC','FB','RDRS_v2_P_TT_09944','UV','RDRS_v2_P_P0_SFC','HU']
-#esmr.var_names_remapped        = ['RDRS_v2.1_P_HU_09944']
+#esmr.var_names_remapped        = ['RDRS_v2_P_HU_09944']
  
 # name of variable longitude in source netCDF files
 esmr.var_lon                  = 'lon'
@@ -43,7 +45,7 @@ esmr.var_lat                  = 'lat'
 # name of variable time in source netCDF file; should be always time
 esmr.var_time                 = 'time'
 # location where the remapped netCDF file will be saved
-esmr.output_dir               = '../../forcing/'
+esmr.output_dir               = '/scratch/calbano/rdrs_output/{}/'.format(sys.argv[1])
 # format of the variables to be saved in remapped files,
 # if one format provided it will be expanded to other variables
 esmr.format_list              = ['f4']
--- Model_Workflow/vector_based_workflow/4_climate_forcing/remapping.sh
+++ Model_Workflow/vector_based_workflow/4_climate_forcing/remapping.sh
@@ -3,12 +3,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=20G
+#SBATCH --array=1980-2018
 #SBATCH --time=04:00:00
-#SBATCH --job-name=remapping_80
+#SBATCH --job-name=remap_78_8198
 #SBATCH --error=errors
 #SBATCH --mail-user=cooper.albano@usask.ca
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
+
 
 # load modules
 module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3
@@ -16,8 +18,8 @@ module load gdal/3.5.1 libspatialindex/1.8.5
 module load python/3.8.10 scipy-stack/2022a mpi4py/3.0.3
 
 # # create and activate virtual environment
-rm -rf $HOME/MESH-env
-virtualenv --no-download $HOME/MESH-env 
+
+virtualenv --no-download $HOME/MESH-env$SLURM_ARRAY_TASK_ID 
 source $HOME/MESH-env/bin/activate 
 
 pip install --no-index --upgrade pip
@@ -28,6 +30,8 @@ pip install --no-index geopandas
 pip install --no-index Rtree
 pip install --no-index easymore
 
-python easymore_remapping.py
-
+#year=$SLURM_ARRAY_TASK_ID
+year=1980
+python easymore_remapping_array.py "$year"
+rm -rf $HOME/MESH-env$SLURM_ARRAY_TASK_ID
 echo  finished
