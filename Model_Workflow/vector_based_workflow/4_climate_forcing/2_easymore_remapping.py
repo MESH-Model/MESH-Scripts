1f098043b52e0d1a8bbd1e9184463eb487123452
cooperalbano <albano.caa@gmail.com>
2022-12-12 11:20:39 -0600

    climate forcing control file update. Basins subset log.

--- Model_Workflow/vector_based_workflow/4_climate_forcing/2_easymore_remapping.py
+++ Model_Workflow/vector_based_workflow/4_climate_forcing/2_easymore_remapping.py
@@ -1,46 +1,104 @@
 #!/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Core/python/3.8.10/bin/python
 # which python to find the location of installed pyhthon on graham
 
-# loading EASYMORE
+# %% loading EASYMORE
 from easymore.easymore import easymore
+from pathlib import Path
 
-# initializing EASYMORE object
+
+# %%Control file handling
+controlFolder = Path('../0_control_files')  # Easy access to control file folder
+controlFile = 'control_active.txt'          # Store the name of the 'active' file in a variable
+
+
+#Function to extract a given setting from the control file
+def read_from_control( file, setting ):
+    # Open 'control_active.txt' and ...
+    with open(file) as contents:
+        for line in contents:
+            # ... find the line with the requested setting
+            if setting in line and not line.startswith('#'):
+                break    
+    # Extract the setting's value
+    substring = line.split('|',1)[1]      # Remove the setting's name (split into 2 based on '|', keep only 2nd part)
+    substring = substring.split('#',1)[0] # Remove comments, does nothing if no '#' is found
+    substring = substring.strip()         # Remove leading and trailing whitespace, tabs, newlines
+    # Return this value   
+    return substring
+
+
+#Function to specify a default path
+def make_default_path(suffix):
+    # Get the root path
+    rootPath = Path( read_from_control(controlFolder/controlFile,'root_path') )
+    defaultPath = rootPath / suffix 
+    return defaultPath
+
+
+# %% Get the domain folder
+domain_name = read_from_control(controlFolder/controlFile,'domain_name')
+domainFolder = 'domain_' + domain_name 
+
+#  %% Get the target shapefile
+target_shp_path = read_from_control(controlFolder/controlFile,'river_basin_shp_path')
+# Specify default path if needed
+if target_shp_path == 'default':
+    target_shp_path = make_default_path('shape_file/catchment/') # outputs a Path()
+else:
+    target_shp_path = Path(target_shp_path) # make sure a user-specified path is a Path()
+target_shp_name = read_from_control(controlFolder/controlFile,'river_basin_shp_name')
+target_shp      = target_shp_path / target_shp_name
+target_shp_ID   = read_from_control(controlFolder/controlFile,'river_basin_shp_rm_hruid')
+
+# %% Get the source forcing file
+source_nc_path  = read_from_control(controlFolder/controlFile,'source_nc_path')
+# Specify default path if needed
+if source_nc_path == 'default':
+    source_nc_path = make_default_path('forcing/NA/yearly/') # outputs a Path()
+else:
+    source_nc_path = Path(source_nc_path) # make sure a user-specified path is a Path()
+source_nc_name  = read_from_control(controlFolder/controlFile,'source_nc_name')
+source_nc     = str(source_nc_path / source_nc_name)
+
+# %% Get the list of variable neames
+var_names       = read_from_control(controlFolder/controlFile,'var_names').split(', ')
+
+
+# %% initializing EASYMORE object
 esmr = easymore()
 # specifying EASYMORE objects
 # name of the case; the temporary, remapping and remapped file names include case name
-esmr.case_name                =  'RDRS_BowAtBanff'
+esmr.case_name                = 'RDRS_{}'.format(domain_name)
 # temporary path that the EASYMORE generated GIS files and remapped file will be saved
-esmr.temp_dir                 =  'temporaryBowAtBanff/'
+esmr.temp_dir                 = 'temporary{}/'.format(domain_name)
 # name of target shapefile that the source netcdf files should be remapped to
-esmr.target_shp               =  '../../shape_file/catchment/BowAtBanff.shp'
+esmr.target_shp               = target_shp
                                 #'../shape_file/cat_pfaf_81_MERIT_Hydro_v07_Basins_v01_bugfix1_WGS84.shp'
-esmr.target_shp_ID            =  'COMID'
+esmr.target_shp_ID            = target_shp_ID
 # name of netCDF file(s); multiple files can be specified with *
-esmr.source_nc                = '../forcing/rdrsv2.1_1980.nc' 
+esmr.source_nc                = source_nc 
                                 #'/project/6008034/baha2501/NA/forcing/NA_RDRS_v2.1_1980010112.nc'
                                 
 # name of variables from source netCDF file(s) to be remapped
-esmr.var_names                = ['RDRS_v2.1_A_PR0_SFC', 'RDRS_v2.1_P_FI_SFC',
-                                  'RDRS_v2.1_P_FB_SFC','RDRS_v2.1_P_TT_09944',
-                                  'RDRS_v2.1_P_UVC_09944','RDRS_v2.1_P_P0_SFC',
-                                  'RDRS_v2.1_P_HU_09944']
+esmr.var_names                = var_names
+
 # rename the variables from source netCDF file(s) in the remapped files;
 # it will be the same as source if not provided
 #esmr.var_names_remapped       = ['RDRS']
 # name of variable longitude in source netCDF files
-esmr.var_lon                  = 'lon'
+esmr.var_lon                  =  read_from_control(controlFolder/controlFile,'var_lon')
 # name of variable latitude in source netCDF files
-esmr.var_lat                  = 'lat'
+esmr.var_lat                  = read_from_control(controlFolder/controlFile,'var_lat')
 # name of variable time in source netCDF file; should be always time
-esmr.var_time                 = 'time'
+esmr.var_time                 = read_from_control(controlFolder/controlFile,'var_time')
 # location where the remapped netCDF file will be saved
 esmr.output_dir               = 'OutputBowAtBanff/'
 # format of the variables to be saved in remapped files,
 # if one format provided it will be expanded to other variables
-esmr.format_list              = ['f4']
+esmr.format_list              = read_from_control(controlFolder/controlFile,'format_list').split(', ')
 # fill values of the variables to be saved in remapped files,
 # if one value provided it will be expanded to other variables
-esmr.fill_value_list          = ['-9999.00']
+esmr.fill_value_list          = read_from_control(controlFolder/controlFile,'fill_value_list').split(', ')
 # if required that the remapped values to be saved as csv as well
 #esmr.save_csv                 = True
 #esmr.complevel                 =  9
@@ -78,3 +136,4 @@ esmr.source_shp_ID             =  'ID_s' # name of column ID in the source shape
 
 # execute EASYMORE
 esmr.nc_remapper()
+# %%
