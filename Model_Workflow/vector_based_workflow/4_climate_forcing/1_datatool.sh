631095da55cbe2a31d353932ae8b0fd865c76fbd
cooperalbano <albano.caa@gmail.com>
2023-01-10 14:45:51 -0600

    added lines to install cdo and nco on workstation machines. Clarified default paths

--- Model_Workflow/vector_based_workflow/4_climate_forcing/1_datatool.sh
+++ Model_Workflow/vector_based_workflow/4_climate_forcing/1_datatool.sh
@@ -16,9 +16,14 @@ rm -rf datatool
 git clone https://github.com/kasra-keshavarz/datatool # clone the repository
 cd datatool
 
+# Uncomment this section to install cdo and nco if working on a workstation machine. These packages are required. Must have sudo authority.
+#sudo apt update
+#sudo apt install cdo
+#sudo apt install nco
+
 ./extract-dataset.sh  --dataset=RDRS \
-  --dataset-dir="/project/rpp-kshook/Model_Output/RDRSv2.1" \
-  --output-dir="$HOME/scratch/rdrs_output/" \
+  --dataset-dir="/mnt/c/Users/<user>/path/to/MESH-Scripts/Model_Workflow/forcing" \
+  --output-dir="/mnt/c/Users/<user>/path/to/MESH-Scripts/Model_Workflow/forcing" \
   --start-date="1980-01-01 00:00:00" \
   --end-date="1980-12-31 00:00:00" \
   --lat-lims=0,89  \
@@ -26,7 +31,7 @@ cd datatool
   --variable="RDRS_v2.1_P_P0_SFC,RDRS_v2.1_P_HU_09944,RDRS_v2.1_P_TT_09944,RDRS_v2.1_P_UVC_09944,RDRS_v2.1_A_PR0_SFC,RDRS_v2.1_P_FB_SFC,RDRS_v2.1_P_FI_SFC" \
   --prefix="rdrsv2.1_" \
   --email="example.email@usask.ca" \
-  -j;
+  -j; # Remove this argument if not submitting to a scheduler on HPC cluster
   
   cd ../scratch
 
