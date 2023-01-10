b95ee2012d258224d7b2cca1244f812afd434efd
cooperalbano <albano.caa@gmail.com>
2023-01-10 14:41:35 -0600

    improved clarity for paths in QGIS Python console

--- Model_Workflow/vector_based_workflow/2_zonal_statistics/2b_landcover_extract/lc_extract.py
+++ Model_Workflow/vector_based_workflow/2_zonal_statistics/2b_landcover_extract/lc_extract.py
@@ -4,11 +4,11 @@
 """
 # %% import module
 import processing
- 
 # %% run the algorithm
-input_raster = '../../landcover.tif'
-input_shape  = '../../MERITHydro_cat_fixgeo.shp'
-output_shpae = '../../domain_name_intersect_NGRU.shp'
+# WARNING: If the paths below are too long, the python console will produce an error. You may need to temporarily store files in such a way that paths are shorter.
+input_raster = 'C:/Users/<user>/path/to/MESH-Scripts/Model_Workflow/vector_based_workflow/workflow_data/domain_BowAtBanff/NA_NALCMS_2010_v2_land_cover_30m.tif'
+input_shape  = 'C:/Users/<user>/path/to/MESH-Scripts/Model_Workflow/shape_file/catchment/BowAtBanff_cat_fg.shp'
+output_shpae = 'C:/Users/<user>/path/to/MESH-Scripts/Model_Workflow/vector_based_workflow/workflow_data/domain_BowAtBanff/zonalhist/BowAtBanff_lczh.shp'
 band           =  1
  
 processing.run("native:zonalhistogram" , {'COLUMN_PREFIX' : 'NALCMS_',
